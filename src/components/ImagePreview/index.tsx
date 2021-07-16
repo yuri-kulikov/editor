8deec7216d74209ad19ae24d1cce44d43ccbd5fb
Yuri Kulikov <yuri.kulikov@akvelon.com>
2021-07-16 16:10:37 +0300

    Refactor SelectedMediaPanelBase

---
+++ src/components/ImagePreview/index.tsx
@@ -0,0 +1,56 @@
+import { Icon, Image, ImageLoadState, IProcessedStyleSet, Label, Link } from '@fluentui/react';
+import React from 'react';
+
+import MyMedia from '@/models/MyMedia';
+
+import { ISelectedMediaPanelStyles } from '../SelectedMediaPanel.types';
+
+interface Props {
+  currentSrc: string | undefined;
+  currentAlt: string | undefined;
+  classNames: IProcessedStyleSet<ISelectedMediaPanelStyles>;
+  selectedImage: MyMedia;
+  setImageState: React.Dispatch<React.SetStateAction<ImageLoadState>>;
+  imageState: ImageLoadState;
+}
+
+const renderError = (errorText, classNames) => {
+  return (
+    <div className={classNames.errorContainer}>
+      <Icon iconName={'ErrorBadge'} className={classNames.errorIcon} />
+      <span className={classNames.errorMessage}>{errorText}</span>
+    </div>
+  );
+};
+
+export const ImagePreview: React.FC<Props> = ({
+  currentSrc,
+  currentAlt,
+  classNames,
+  selectedImage,
+  setImageState,
+  imageState,
+}) => {
+  return (
+    <>
+      <Label>Image Preview</Label>
+      <Image
+        alt={currentAlt}
+        className={classNames.image}
+        key={selectedImage.src}
+        src={currentSrc}
+        width={'fit-content'}
+        onLoadingStateChange={setImageState}
+        style={imageState === ImageLoadState.error ? { display: 'none' } : {}}
+      />
+
+      {imageState === ImageLoadState.loaded && (
+        <Link target={'_blank'} href={currentSrc}>
+          Open image in a new window
+        </Link>
+      )}
+      {imageState === ImageLoadState.error &&
+        renderError("Couldn't load the image", classNames)}
+    </>
+  );
+};
--- src/components/SelectedMediaPanel.base.tsx
+++ src/components/SelectedMediaPanel.base.tsx
@@ -1,11 +1,7 @@
 import {
   classNamesFunction,
-  Icon,
-  Image,
   ImageLoadState,
   IProcessedStyleSet,
-  Label,
-  Link,
   Panel,
   PanelType,
   PrimaryButton,
@@ -19,6 +15,7 @@ import MyMedia from '@/models/MyMedia';
 import { MyMediaCommandName } from '@/plugins/MyMedia/MyMediaEditing';
 import cx from '@/utils/classNames';
 
+import { ImagePreview } from './ImagePreview';
 import {
   ISelectedMediaPanelProps,
   ISelectedMediaPanelStyleProps,
@@ -30,129 +27,111 @@ const getClassNames = classNamesFunction<
   ISelectedMediaPanelStyles
 >();
 
-export const SelectedMediaPanelBase: React.FC<ISelectedMediaPanelProps> =
-  props => {
-    const editor = useContext(EditorContext);
-    const selectedImageContext = useContext(SelectedMediaContext);
+export const SelectedMediaPanelBase: React.FC<ISelectedMediaPanelProps> = ({
+  styles,
+  theme,
+}) => {
+  const editor = useContext(EditorContext);
+  const selectedImageContext = useContext(SelectedMediaContext);
 
-    const [currentSrc, setCurrentSrc] = useState<string | undefined>(
-      selectedImageContext?.selectedMedia?.src,
-    );
-    const [currentAlt, setCurrentAlt] = useState<string | undefined>(
-      selectedImageContext?.selectedMedia?.alt,
-    );
+  const [currentSrc, setCurrentSrc] = useState<string | undefined>(
+    selectedImageContext?.selectedMedia?.src,
+  );
+  const [currentAlt, setCurrentAlt] = useState<string | undefined>(
+    selectedImageContext?.selectedMedia?.alt,
+  );
 
-    const [imageState, setImageState] = useState(ImageLoadState.notLoaded);
+  const [imageState, setImageState] = useState(ImageLoadState.notLoaded);
 
-    if (!selectedImageContext) {
-      return null;
-    }
-    const {
-      selectedMedia: selectedImage,
-      panelDismissed,
-      setPanelDismissed,
-    } = selectedImageContext;
-    if (!selectedImage) {
-      return null;
-    }
-    const { src, alt, path } = selectedImage;
+  if (!selectedImageContext) {
+    return null;
+  }
+  const {
+    selectedMedia: selectedImage,
+    panelDismissed,
+    setPanelDismissed,
+  } = selectedImageContext;
+  if (!selectedImage) {
+    return null;
+  }
+  const { src, alt, path } = selectedImage;
 
-    if (!path || panelDismissed) {
-      return null;
-    }
+  if (!path || panelDismissed) {
+    return null;
+  }
 
-    const { styles, theme } = props;
-    const classNames: IProcessedStyleSet<ISelectedMediaPanelStyles> =
-      getClassNames(styles, {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        theme: theme!,
-      });
+  const classNames: IProcessedStyleSet<ISelectedMediaPanelStyles> =
+    getClassNames(styles, {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      theme: theme!,
+    });
 
-    const updateSelectedImage = ({ src, alt }: Partial<MyMedia>) => {
-      editor?.execute(MyMediaCommandName.UpdateSelectedImage, {
-        src,
-        alt,
-      });
-    };
-
-    const onSave = () => {
-      updateSelectedImage({
-        src: currentSrc,
-        alt: currentAlt,
-      });
-      setPanelDismissed(true);
-    };
-
-    const onRenderFooterContent = () => (
-      <div className={classNames.footerButtonsContainer}>
-        <PrimaryButton
-          disabled={!currentSrc}
-          onClick={onSave}
-          className={classNames.button}
-        >
-          Save
-        </PrimaryButton>
-        <PrimaryButton
-          onClick={() => {
-            editor?.execute('delete');
-          }}
-          className={cx(classNames.button, classNames.removeButton)}
-        >
-          Remove Image
-        </PrimaryButton>
-      </div>
-    );
-
-    return (
-      <Panel
-        key={selectedImage.path}
-        closeButtonAriaLabel="Close"
-        customWidth={'400px'}
-        headerText="Image settings"
-        isBlocking={false}
-        isOpen={!!selectedImage}
-        type={PanelType.custom}
-        onDismiss={() => setPanelDismissed(true)}
-        onRenderFooterContent={onRenderFooterContent}
-        isFooterAtBottom={true}
-      >
-        <TextField
-          label="Source"
-          defaultValue={src}
-          onChange={(_, src) => setCurrentSrc(src)}
-        />
-        <TextField
-          label="Alt Text"
-          defaultValue={alt || ''}
-          onChange={(_, alt) => setCurrentAlt(alt)}
-        />
-        <Label>Image Preview</Label>
-        {!!currentSrc && (
-          <Image
-            alt={currentAlt}
-            className={classNames.image}
-            key={selectedImage.src}
-            src={currentSrc}
-            width={'fit-content'}
-            onLoadingStateChange={setImageState}
-            style={
-              imageState === ImageLoadState.error ? { display: 'none' } : {}
-            }
-          />
-        )}
-        {!!currentSrc && imageState === ImageLoadState.loaded && (
-          <Link target={'_blank'} href={currentSrc}>
-            Open image in a new window
-          </Link>
-        )}
-        {!!currentSrc && imageState === ImageLoadState.error && (
-          <div className={classNames.errorContainer}>
-            <Icon iconName={'ErrorBadge'} className={classNames.errorIcon} />
-            <span className={classNames.errorMessage}>
-              Couldn't load the image
-            </span>
-          </div>
-        )}
-      </Panel>
-    );
+  const updateSelectedImage = ({ src, alt }: Partial<MyMedia>) => {
+    editor?.execute(MyMediaCommandName.UpdateSelectedImage, {
+      src,
+      alt,
+    });
   };
+
+  const onSave = () => {
+    updateSelectedImage({
+      src: currentSrc,
+      alt: currentAlt,
+    });
+    setPanelDismissed(true);
+  };
+
+  const onRenderFooterContent = () => (
+    <div className={classNames.footerButtonsContainer}>
+      <PrimaryButton
+        disabled={!currentSrc}
+        onClick={onSave}
+        className={classNames.button}
+      >
+        Save
+      </PrimaryButton>
+      <PrimaryButton
+        onClick={() => {
+          editor?.execute('delete');
+        }}
+        className={cx(classNames.button, classNames.removeButton)}
+      >
+        Remove Image
+      </PrimaryButton>
+    </div>
+  );
+
+  return (
+    <Panel
+      key={selectedImage.path}
+      closeButtonAriaLabel="Close"
+      customWidth={'400px'}
+      headerText="Image settings"
+      isBlocking={false}
+      isOpen={!!selectedImage}
+      type={PanelType.custom}
+      onDismiss={() => setPanelDismissed(true)}
+      onRenderFooterContent={onRenderFooterContent}
+      isFooterAtBottom={true}
+    >
+      <TextField
+        label="Source"
+        defaultValue={src}
+        onChange={(_, src) => setCurrentSrc(src)}
+      />
+      <TextField
+        label="Alt Text"
+        defaultValue={alt || ''}
+        onChange={(_, alt) => setCurrentAlt(alt)}
+      />
+      <ImagePreview
+        classNames={classNames}
+        currentAlt={currentAlt}
+        currentSrc={currentSrc}
+        imageState={imageState}
+        selectedImage={selectedImage}
+        setImageState={setImageState}
+      />
+    </Panel>
+  );
+};
