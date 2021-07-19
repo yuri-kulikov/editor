b8130e457114da54f222bd987dcba66e860a4658
Yuri Kulikov <yuri.kulikov@akvelon.com>
2021-07-19 20:08:00 +0300

    Move global styles into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { mergeStyles } from '@fluentui/merge-styles';
 import * as React from 'react';
 
 import { styled } from '@fluentui/react/lib/Utilities';
@@ -5,6 +6,9 @@ import { styled } from '@fluentui/react/lib/Utilities';
 import AppBase from './App.base';
 import { getStyles } from './App.styles';
 import { IAppProps, IAppStyleProps, IAppStyles } from './App.types';
+import globalStyles from './styles/global';
+
+mergeStyles(globalStyles);
 
 export const App: React.FC = styled<IAppProps, IAppStyleProps, IAppStyles>(
   AppBase,
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import { mergeStyles } from '@fluentui/react';
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import globalStyles from '@/styles/global';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 
 import App from './App';
 
 initializeIcons();
-
-mergeStyles(globalStyles);
 
 ReactDOM.render(<App />, document.getElementById('root'));
